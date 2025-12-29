11e69647940d51b1a08fbe21c35f5664dc1cf412
Nipuna H Herath <nipunaherath@hotmail.com>
2025-12-29 12:11:17 +0800

    add player rotation and camera follow

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+@export var speed: float = 5.0
 const JUMP_VELOCITY = 4.5
-
+@onready var camera: Node3D = $CameraRig/Camera3D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,12 +17,22 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (camera.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	direction = Vector3(direction.x,0,direction.z).normalized() *input_dir.length()
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+	turn_to(direction)
+
+func turn_to(direction: Vector3)-> void:
+	if direction:
+		var yaw := atan2(-direction.x,-direction.z)
+		yaw = lerp_angle(rotation.y,yaw, 0.25)
+		rotation.y = yaw 
